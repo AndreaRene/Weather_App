ed0a24000d802a8035196eac036273337591ebf7
Andrea <andrea@interhact.net>
2022-08-01 17:11:55 -0500

    app functional. add styling. figure out non city searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
 var key1 = "3d1d22fa9f3fdc267f07adb7bc963172";
 var date = moment().format("MM/DD/YYYY");
+var cityArray = JSON.parse(localStorage.getItem("city")) || [];
 
 var searchHandler = function (event) {
     event.preventDefault();
+    if ($(this).attr("id") === "fetchBtn") {
+        var city = $("#citySearch").val();
 
-    var city = $("#citySearch").val();
+        if (city) {
+            search(city);
+            storeCity();
 
-    if (city) {
-        search(city);
-        storeCity();
-
+        } else {
+            alert('Please enter a city');
+        }
     } else {
-        alert('Please enter a city');
-    }
+        var city = $(this).text();
+        search(city);
+    };
 };
 
 function search(city) {
@@ -25,7 +30,7 @@ function search(city) {
                 .then((response) => {
                     return response.json();
                 }).then((data) => {
-                    $("#currentCity").text(city + " " + "(" + date + ")");
+                    $("#currentCity").text(city + " " + "(" + moment.unix(data.current.dt).format("MM/DD/YYYY") + ")");
                     $("#currentIcon").attr("src", "https://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png")
                     $("#currentTemp").text(" " + Math.floor(data.current.temp) + "\u00B0 F");
                     $("#currentWind").text(" " + data.current.wind_speed + " MPH");
@@ -35,27 +40,58 @@ function search(city) {
                     console.log(data);
 
                     let i = 0;
+                    let j = 0;
+                    let k = 0;
+                    let l = 0;
+                    let m = 0;
 
-                    $(".future").each(function () {
-                        $(".futureTemp").text(" " + data.daily[i].temp.day);
+                    $(".futureTemp").each(function () {
+                        $(this).text(" " + Math.floor(data.daily[i].temp.day));
                         i++;
+                    });
+                    $(".futureWind").each(function () {
+                        $(this).text(" " + data.daily[j].wind_speed);
+                        j++;
+                    });
+                    $(".futureHumidity").each(function () {
+                        $(this).text(" " + data.daily[k].humidity);
+                        k++;
+                    });
+                    $(".futureDate").each(function () {
+                        $(this).text(moment.unix(data.daily[l].dt).format("MM/DD/YYYY"));
+                        l++;
+                    });
+                    $(".futureIcon").each(function () {
+                        $(this).attr("src", "https://openweathermap.org/img/wn/" + data.daily[m].weather[0].icon + ".png");
+                        m++;
                     });
                 });
         });
-}
+};
 
 function storeCity() {
-    localStorage.setItem("city", $("#citySearch").val());
-}
+    cityArray.push($("#citySearch").val());
+    localStorage.setItem("city", JSON.stringify(cityArray));
+    makeButtons();
 
+};
+
+function makeButtons() {
+    $("#searchHistory").empty();
+    for (var i = 0; i < cityArray.length; i++) {
+        var button = $("<button>").text(cityArray[i]);
+        $("#searchHistory").append(button);
+    };
+};
+
+function clearStorage() {
+    localStorage.clear();
+    location.reload();
+};
 
 $("#fetchBtn").click(searchHandler);
-// function getWeather(data) {
-//     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&exclude=hourly&appid=" + key2)
-//         .then((response) => {
-//             return response.json();
-//         }).then((data) => {
-//             console.log(data);
-//         });
-// }
+$("#clear").click(clearStorage);
+$("#searchHistory").on("click", "button", searchHandler);
+makeButtons();
+
 
