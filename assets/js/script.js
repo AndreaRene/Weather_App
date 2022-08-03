d4a804c9c5a39beb5e62e8debf4bde0fd130d8ac
Andrea <andrea@interhact.net>
2022-08-02 21:14:14 -0500

    date issue found and resolved. working on responsive design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // TODO: Smile. You are enough.
 var key1 = "3d1d22fa9f3fdc267f07adb7bc963172";
-var date = moment().format("MM/DD/YYYY");
 var cityArray = JSON.parse(localStorage.getItem("city")) || [];
 
 var searchHandler = function (event) {
@@ -27,7 +26,6 @@ function search(city) {
         .then((response) => {
             return response.json();
         }).then((data) => {
-            console.log("HERE", data)
             if (!data.length) {
                 return alert("Please enter a valid city.");
             }
@@ -36,19 +34,17 @@ function search(city) {
                 .then((response) => {
                     return response.json();
                 }).then((data) => {
-                    $("#currentCity").text(city + " " + "(" + moment.unix(data.current.dt).format("MM/DD/YYYY") + ")");
-                    $("#currentIcon").attr("src", "https://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png")
+                    $("#currentCity").text(city + " " + "(" + moment.unix(data.current.dt).utcOffset(data.timezone_offset / 60).format("MM/DD/YYYY") + ")");
+                    $("#currentIcon").attr("src", "https://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png");
                     $("#currentTemp").text(" " + Math.floor(data.current.temp) + "\u00B0 F");
                     $("#currentWind").text(" " + data.current.wind_speed + " MPH");
                     $("#currentHumidity").text(" " + data.current.humidity + "\%");
                     $("#currentUV").text(" " + data.current.uvi)
 
-
-
                     let i = 0;
                     let j = 0;
                     let k = 0;
-                    let l = 0;
+                    let l = 1;
                     let m = 0;
 
                     $(".futureTemp").each(function () {
@@ -64,7 +60,8 @@ function search(city) {
                         k++;
                     });
                     $(".futureDate").each(function () {
-                        $(this).text(moment.unix(data.daily[l].dt).format("MM/DD/YYYY"));
+                        let date = data.daily[l].dt + data.timezone_offset;
+                        $(this).text(moment.unix(date).utcOffset(data.timezone_offset / 60).format("MM/DD/YYYY"));
                         l++;
                     });
                     $(".futureIcon").each(function () {
@@ -90,7 +87,7 @@ function storeCity() {
 function makeButtons() {
     $("#searchHistory").empty();
     for (var i = 0; i < cityArray.length; i++) {
-        var button = $("<button>").addClass("myBtns").text(cityArray[i]);
+        var button = $("<button>").addClass("myBtns cityBtns").text(cityArray[i]);
         $("#searchHistory").append(button);
     };
 };
